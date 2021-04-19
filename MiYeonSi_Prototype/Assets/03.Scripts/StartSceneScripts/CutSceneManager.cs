7f50864b11a1a268c286b3405f2ddb6cc2779a3b
Kimsol <ksol92789@gmail.com>
2021-04-20 00:24:31 +0900

    시작화면 UI 제작중
    UI이미지 추가, 씬 전환 제작 (이미지는 추후에 아틀라스로 묶을예정)

---
+++ MiYeonSi_Prototype/Assets/03.Scripts/StartSceneScripts/CutSceneManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutSceneManager : MonoBehaviour
+{
+    private float animationPlayTime = 4.0f;
+    private int i = 0;
+
+    public GameObject[] CutScenes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(MoveToNext());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator MoveToNext()
+    {
+        while (i < CutScenes.Length-1)
+        {
+            yield return new WaitForSeconds(animationPlayTime);
+
+            CutScenes[i].SetActive(false);
+            CutScenes[i+1].SetActive(true);
+
+            i++;
+        }
+        i = 0;
+    }
+}
