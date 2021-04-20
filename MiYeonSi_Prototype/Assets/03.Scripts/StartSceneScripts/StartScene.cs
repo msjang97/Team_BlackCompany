79e6128f254906e53caa726153e8a2755775e5aa
Kimsol <ksol92789@gmail.com>
2021-04-20 22:36:22 +0900

    시작화면 UI 끝
    엔딩모음, 이어하기는 추후 UI가 나오면 제작.

---
+++ MiYeonSi_Prototype/Assets/03.Scripts/StartSceneScripts/StartScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScene : MonoBehaviour
+{  
+    public void LoadGameScene() //게임 시작
+    {
+        SceneManager.LoadScene("MainSystem");
+    }
+
+    public void QuitApplication() //게임 종료
+    {
+        Application.Quit();
+    }
+    
+}
