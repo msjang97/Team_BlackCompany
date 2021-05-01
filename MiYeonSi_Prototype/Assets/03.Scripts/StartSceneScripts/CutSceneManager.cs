e959bdf4655df2450a34bdc57d591cf077db0dfa
Kimsol <ksol92789@gmail.com>
2021-05-01 19:57:33 +0900

    메인화면 오류 수정
    1. 인게임에서 메인메뉴으로 이동했을 때 익스디오 로고에서 안움직이던 오류 수정-> 바로 메인메뉴로 이동.
    2. 엔딩모음 화면은 다 비공개 이미지로 바꿔놓음.

--- MiYeonSi_Prototype/Assets/03.Scripts/StartSceneScripts/CutSceneManager.cs
+++ MiYeonSi_Prototype/Assets/03.Scripts/StartSceneScripts/CutSceneManager.cs
@@ -6,13 +6,20 @@ public class CutSceneManager : MonoBehaviour
 {
     private float animationPlayTime = 1.0f;
     private int i = 0;
+    private static bool isCutScene = true;
 
     public GameObject[] CutScenes;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(MoveToNext());
+        if (isCutScene == true)
+        {
+            StartCoroutine(MoveToNext());
+            isCutScene = false;
+        }
+        else
+            ShowMainMenu.instance.ActiveMainMenu();
     }
 
     // Update is called once per frame
---
+++ MiYeonSi_Prototype/Assets/03.Scripts/StartSceneScripts/ShowMainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowMainMenu : MonoBehaviour
+{
+    public static ShowMainMenu instance;
+    private GameObject MainMenu;
+
+    private void Awake()
+    {
+        instance = this;
+        MainMenu = this.transform.GetChild(3).gameObject;        
+    }
+
+    public void ActiveMainMenu()
+    {
+        MainMenu.SetActive(true);
+    }
+}
