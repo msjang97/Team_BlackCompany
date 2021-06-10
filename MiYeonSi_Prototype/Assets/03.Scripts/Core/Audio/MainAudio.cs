70955c339db8a393753996b14b1aedc273ec9240
jwnsqud <jwnsqud@gmail.com>
2021-06-10 16:32:15 +0900

    메인화면 배경음악 수정
    제곧내

---
+++ MiYeonSi_Prototype/Assets/03.Scripts/Core/Audio/MainAudio.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainAudio : MonoBehaviour
+{
+    public GameObject title;
+    public GameObject main_menu;
+
+
+    private AudioSource audio;
+
+    bool check = false; 
+
+    private static MainAudio instance = null;
+    public static MainAudio P_instance
+    {
+        get
+        {
+            if (null == instance)
+            {
+                return null;
+            }
+            return instance;
+        }
+    }
+    private void Awake()
+    {
+        if (null == instance)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        
+
+    }
+
+    private void Start()
+    {
+        audio = gameObject.GetComponent<AudioSource>();
+        //audio.playOnAwake = false;
+        audio.Stop();
+        check = false;
+    }
+
+    private void Update()
+    {    
+        if (title.activeSelf == true )//|| main_menu.activeSelf == true)
+        {
+            check = true;
+        }
+        if (check == true)
+        {
+            //audio.playOnAwake = true;
+            audio.Play();
+            check = false;
+        }
+     
+    }
+
+}
